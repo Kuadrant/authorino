4702d7c2ecfa4f8c2b7308f08e91b2b4e56380d7
Guilherme Cassolato <guicassolato@gmail.com>
2020-12-22 13:01:59 +0100

    Remove legacy PoCs
    They can still be acessed at https://github.com/3scale-labs/authorino-rb

--- pocs/ruby-keycloak-authz/lib/keycloak_adapter.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module KeycloakAdapter
-  class AuthorizationError < StandardError; end
-  class MissingTokenError < AuthorizationError; end
-end
-
-Dir[Rails.root.join('lib', 'keycloak_adapter', '*.rb')].each { |file| require file }
--- pocs/ruby-keycloak-authz/lib/keycloak_adapter/parsed_jwt.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module KeycloakAdapter
-  class ParsedJWT < Struct.new(:payload, :header)
-  end
-end
