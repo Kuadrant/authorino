691886405ee9e7d301108a078ac9209eb5b27ba3
Joaquim Moreno <jmprusi@keepalive.io>
2021-01-07 16:13:05 +0100

    Basic config cache for storing the current config state

---
+++ pkg/cache/cache.go
@@ -0,0 +1,61 @@
+package cache
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/3scale-labs/authorino/pkg/config"
+)
+
+type Cache struct {
+	// TODO: move to RWMutex?
+	mu         sync.Mutex
+	keyMapping map[string][]string
+	apiConfigs map[string]config.APIConfig
+}
+
+func NewCache() Cache {
+	return Cache{
+		mu:         sync.Mutex{},
+		keyMapping: make(map[string][]string),
+		apiConfigs: make(map[string]config.APIConfig),
+	}
+
+}
+
+func (c *Cache) Get(key string) config.APIConfig {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.apiConfigs[key]
+}
+
+func (c *Cache) Set(key string, serviceHost string, config config.APIConfig, override bool) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if _, ok := c.apiConfigs[serviceHost]; !ok || override {
+		c.apiConfigs[serviceHost] = config
+	} else {
+		return fmt.Errorf("service already exists in cache: %s", serviceHost)
+	}
+	c.keyMapping[key] = append(c.keyMapping[key], serviceHost)
+
+	return nil
+}
+
+func (c *Cache) List() map[string]config.APIConfig {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.apiConfigs
+}
+
+func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for _, configName := range c.keyMapping[key] {
+		delete(c.apiConfigs, configName)
+	}
+}
