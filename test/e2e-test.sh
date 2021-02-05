66fb86ccab7bda51d528b25fb54f442ce52559a5
Joaquim Moreno <jmprusi@keepalive.io>
2021-02-05 21:34:59 +0100

    test

--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -26,13 +26,13 @@ fi
 trap 'handle_error $LINENO ${BASH_LINENO[@]}' ERR
 
 
-#kubectl -n authorino create secret generic userinfosecret \
-#--from-literal=clientID=authorino \
-#--from-literal=clientSecret='2e5246f2-f4ef-4d55-8225-36e725071dee'
+kubectl -n authorino create secret generic userinfosecret \
+--from-literal=clientID=authorino \
+--from-literal=clientSecret='2e5246f2-f4ef-4d55-8225-36e725071dee'
 
-#kubectl -n authorino create secret generic umacredentialssecret \
-#--from-literal=clientID=pets-api \
-#--from-literal=clientSecret='523b92b6-625d-4e1e-a313-77e7a8ae4e88'
+kubectl -n authorino create secret generic umacredentialssecret \
+--from-literal=clientID=pets-api \
+--from-literal=clientSecret='523b92b6-625d-4e1e-a313-77e7a8ae4e88'
 
 kubectl apply -f ./config/samples/config_v1beta1_service.yaml -n authorino
 
@@ -43,20 +43,24 @@ launch_as envoy_logs kubectl logs -n authorino deployments/envoy &
 launch_as authorino_logs kubectl logs -n authorino deployments/authorino-controller-manager -c manager &
 
 # TODO: This should actually wait for the status of the created Service to be set to ready
-kubectl port-forward --namespace authorino deployment/envoy 8081:8000 &
+kubectl port-forward --namespace authorino deployment/envoy 8000:8000 &
 kubectl port-forward --namespace authorino deployment/keycloak 8080:8080 &
 
-#sleep 60
+sleep 60
+
+echo "curl"
+curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8000/pets || true
+echo "end"
 
 export ACCESS_TOKEN_JOHN=$(curl -k -d 'grant_type=password' -d 'client_id=demo' -d 'username=john' -d 'password=p' "http://localhost:8080/auth/realms/ostia/protocol/openid-connect/token" | jq -r '.access_token')
 
-[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8081/pets) -eq 200 ];
-[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8081/pets/1) -eq 200 ];
-[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8081/stats) -eq 401 ];
+[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8000/pets) -eq 200 ];
+[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8000/pets/1) -eq 200 ];
+[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8000/stats) -eq 401 ];
 
 export ACCESS_TOKEN_JANE=$(curl -k -d 'grant_type=password' -d 'client_id=demo' -d 'username=jane' -d 'password=p' "http://localhost:8080/auth/realms/ostia/protocol/openid-connect/token" | jq -r '.access_token')
 
-[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8081/pets) -eq 200 ];
-[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8081/pets/1) -eq 200 ];
-[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8081/stats) -eq 401 ];
+[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8000/pets) -eq 200 ];
+[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8000/pets/1) -eq 200 ];
+[ $(curl -o /dev/null -L -s -w "%{http_code}" -H 'Host: echo-api' -H "Authorization: Bearer $ACCESS_TOKEN_JOHN" http://localhost:8000/stats) -eq 401 ];
 
