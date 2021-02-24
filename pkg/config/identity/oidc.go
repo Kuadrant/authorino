5a74297b3b4240852a2d27597758f72f35ad7650
Guilherme Cassolato <guicassolato@gmail.com>
2021-02-24 19:28:03 +0100

    Check context before each most heavy step of OIDC.Call

--- pkg/config/identity/oidc.go
+++ pkg/config/identity/oidc.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/3scale-labs/authorino/pkg/common"
 
-	oidc "github.com/coreos/go-oidc"
+	goidc "github.com/coreos/go-oidc"
 )
 
 type OIDC struct {
@@ -13,38 +13,66 @@ type OIDC struct {
 	Endpoint string `yaml:"endpoint"`
 }
 
-func (self *OIDC) Call(authContext common.AuthContext, ctx context.Context) (interface{}, error) {
-	// extract access token
+func (oidc *OIDC) Call(authContext common.AuthContext, ctx context.Context) (interface{}, error) {
+	// discover oidc config
+	// TODO: Move to a 'prepare' step and cache it (like in pkg/config/authorization/opa.go)
+	provider, err := oidc.NewProvider(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// retrieve access token
 	accessToken, err := authContext.AuthorizationToken()
 	if err != nil {
 		return nil, err
 	}
 
-	// verify jwt
-	provider, err := self.NewProvider(ctx)
-	if err != nil {
-		return nil, err
-	}
-	oidcConfig := &oidc.Config{SkipClientIDCheck: true, SkipIssuerCheck: true}
-	verifier := provider.Verifier(oidcConfig)
-	idToken, err := verifier.Verify(ctx, accessToken)
-	if err != nil {
-		return nil, err
-	}
-
+	// verify jwt and extract claims
 	var claims interface{}
-	err = idToken.Claims(&claims)
-	if err != nil {
+	if _, err := oidc.decodeAndVerifyToken(provider, accessToken, ctx, &claims); err != nil {
 		return nil, err
+	} else {
+		return claims, nil
 	}
-
-	return claims, nil
 }
 
-func (self *OIDC) NewProvider(ctx context.Context) (*oidc.Provider, error) {
-	provider, err := oidc.NewProvider(ctx, self.Endpoint)
+func (oidc *OIDC) NewProvider(ctx context.Context) (*goidc.Provider, error) {
+	if err := common.CheckContext(ctx); err != nil {
+		return nil, err
+	}
+
+	if provider, err := goidc.NewProvider(ctx, oidc.Endpoint); err != nil {
+		return nil, err
+	} else {
+		return provider, nil
+	}
+}
+
+func (oidc *OIDC) verifyToken(provider *goidc.Provider, accessToken string, ctx context.Context) (*goidc.IDToken, error) {
+	oidcConfig := &goidc.Config{SkipClientIDCheck: true, SkipIssuerCheck: true}
+
+	if idToken, err := provider.Verifier(oidcConfig).Verify(ctx, accessToken); err != nil {
+		return nil, err
+	} else {
+		return idToken, nil
+	}
+}
+
+func (oidc *OIDC) decodeAndVerifyToken(provider *goidc.Provider, accessToken string, ctx context.Context, claims *interface{}) (*goidc.IDToken, error) {
+	if err := common.CheckContext(ctx); err != nil {
+		return nil, err
+	}
+
+	// verify jwt
+	idToken, err := oidc.verifyToken(provider, accessToken, ctx)
 	if err != nil {
 		return nil, err
 	}
-	return provider, nil
+
+	// extract claims
+	if err := idToken.Claims(&claims); err != nil {
+		return nil, err
+	}
+
+	return idToken, nil
 }
