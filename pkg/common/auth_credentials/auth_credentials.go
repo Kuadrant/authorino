f882dd6770bb74e2bec8e1a5064d092081812444
dd di cesare <didi@posteo.net>
2021-03-12 23:31:35 +0700

    [auth creds] Refactor: fix auth headers + GetCredentialsFromCluster func
    * As defined in envoy proxy v3: All header keys must be lower-cased, because HTTP header keys are case-insensitive
    * Passing the Client.Reader as part of the struct to fetch secrets from
      cluster

--- pkg/common/auth_credentials/auth_credentials.go
+++ pkg/common/auth_credentials/auth_credentials.go
@@ -1,33 +1,51 @@
 package auth_credentials
 
 import (
+	"context"
 	"fmt"
 	"regexp"
 	"strings"
 
 	envoyServiceAuthV3 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
+	v1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type AuthCredentials interface {
 	GetCredentialsFromReq(*envoyServiceAuthV3.AttributeContext_HttpRequest) (string, error)
+	GetCredentialsFromCluster(context.Context, map[string]string) ([]string, error)
 }
 
 type AuthCredential struct {
 	KeySelector string `yaml:"key_selector"`
 	In          string `yaml:"in"`
+	k8sClient   client.Reader
 }
 
 const (
 	inCustomHeader = "custom_header"
 	inAuthHeader   = "authorization_header"
 	inQuery        = "query"
+
+	credentialNotFoundMsg          = "credential not found"
+	credentialNotFoundInHeaderMsg  = "the credential was not found in the request header"
+	credentialLocationNotSupported = "the credential location is not supported"
+	authHeaderNotSetMsg            = "the Authorization header is not set"
 )
 
 var (
 	authCredLog = ctrl.Log.WithName("Authorino").WithName("AuthCredential")
-	notFoundErr = fmt.Errorf("credential not found")
+	notFoundErr = fmt.Errorf(credentialNotFoundMsg)
 )
+
+func NewAuthCredential(selector string, location string, k8sClient client.Reader) *AuthCredential {
+	return &AuthCredential{
+		selector,
+		location,
+		k8sClient,
+	}
+}
 
 func (c *AuthCredential) GetCredentialsFromReq(httpReq *envoyServiceAuthV3.AttributeContext_HttpRequest) (string, error) {
 	switch c.In {
@@ -38,14 +56,29 @@ func (c *AuthCredential) GetCredentialsFromReq(httpReq *envoyServiceAuthV3.Attri
 	case inQuery:
 		return getCredFromQuery(httpReq.GetPath(), c.KeySelector)
 	default:
-		return "", fmt.Errorf("the credential location is not supported")
+		return "", fmt.Errorf(credentialLocationNotSupported)
 	}
 }
 
+func (c *AuthCredential) GetCredentialsFromCluster(ctx context.Context, labelSelectors map[string]string) ([]string, error) {
+	var matchingLabels client.MatchingLabels = labelSelectors
+	var secretList = &v1.SecretList{}
+	if err := c.k8sClient.List(ctx, secretList, matchingLabels); err != nil {
+		return nil, err
+	}
+	var parsedSecrets []string
+
+	for _, secret := range secretList.Items {
+		parsedSecrets = append(parsedSecrets, string(secret.Data[c.KeySelector]))
+	}
+
+	return parsedSecrets, nil
+}
+
 func getCredFromCustomHeader(headers map[string]string, keyName string) (string, error) {
-	cred, ok := headers[keyName]
+	cred, ok := headers[strings.ToLower(keyName)]
 	if !ok {
-		authCredLog.Error(notFoundErr, "the credential was not found in the request header")
+		authCredLog.Error(notFoundErr, credentialNotFoundInHeaderMsg)
 		return "", notFoundErr
 	}
 	return cred, nil
@@ -54,7 +87,7 @@ func getCredFromAuthHeader(headers map[string]string, keyName string) (string, e
 	authHeader, ok := headers["authorization"]
 
 	if !ok {
-		authCredLog.Error(notFoundErr, "the Authorization header is not set")
+		authCredLog.Error(notFoundErr, authHeaderNotSetMsg)
 		return "", notFoundErr
 	}
 	prefix := keyName + " "
--- pkg/common/auth_credentials/auth_credentials_test.go
+++ pkg/common/auth_credentials/auth_credentials_test.go
@@ -1,7 +1,14 @@
 package auth_credentials
 
 import (
+	"context"
+	"fmt"
 	"testing"
+
+	v1 "k8s.io/api/core/v1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	envoyServiceAuthV3 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
 	"gotest.tools/assert"
@@ -11,6 +18,16 @@ func TestConstants(t *testing.T) {
 	assert.Check(t, "custom_header" == inCustomHeader)
 	assert.Check(t, "authorization_header" == inAuthHeader)
 	assert.Check(t, "query" == inQuery)
+	assert.Check(t, "credential not found" == credentialNotFoundMsg)
+	assert.Check(t, "the credential was not found in the request header" == credentialNotFoundInHeaderMsg)
+	assert.Check(t, "the credential location is not supported" == credentialLocationNotSupported)
+	assert.Check(t, "the Authorization header is not set" == authHeaderNotSetMsg)
+}
+
+func TestNewAuthCredential(t *testing.T) {
+	creds := NewAuthCredential("api_key", "query", nil)
+	assert.Check(t, creds.KeySelector == "api_key")
+	assert.Check(t, creds.In == "query")
 }
 
 func TestGetCredentialsLocationNotSupported(t *testing.T) {
@@ -26,7 +43,7 @@ func TestGetCredentialsLocationNotSupported(t *testing.T) {
 
 func TestGetCredentialsFromCustomHeaderSuccess(t *testing.T) {
 	var httpReq = envoyServiceAuthV3.AttributeContext_HttpRequest{
-		Headers: map[string]string{"X-API-KEY": "DasUberApiKey"},
+		Headers: map[string]string{"x-api-key": "DasUberApiKey"},
 	}
 
 	authCredentials := AuthCredential{
@@ -118,4 +135,50 @@ func TestGetCredentialsFromQueryFail(t *testing.T) {
 	_, err := authCredentials.GetCredentialsFromReq(&httpReq)
 
 	assert.Error(t, err, "credential not found")
+}
+
+type MockK8sClient struct{}
+
+var (
+	listSecretsFunc func(*v1.SecretList) error
+)
+
+func (k *MockK8sClient) Get(ctx context.Context, key client.ObjectKey, obj runtime.Object) error {
+	return nil
+}
+
+func (k *MockK8sClient) List(ctx context.Context, list runtime.Object, opts ...client.ListOption) error {
+	return listSecretsFunc(list.(*v1.SecretList))
+}
+
+var mockK8sClient MockK8sClient
+
+func TestGetCredentialsFromClusterSuccess(t *testing.T) {
+	listSecretsFunc = func(list *v1.SecretList) error {
+		var secrets []v1.Secret
+		secrets = append(
+			secrets,
+			v1.Secret{Data: map[string][]byte{"X-API-KEY": []byte("DasUberApiKey")}},
+			v1.Secret{Data: map[string][]byte{"X-API-KEY": []byte("DasUberApiKey2")}},
+		)
+		list.Items = append(list.Items, secrets...)
+		return nil
+	}
+	authCred := NewAuthCredential("X-API-KEY", "custom_header", &mockK8sClient)
+	creds, err := authCred.GetCredentialsFromCluster(context.TODO(), map[string]string{})
+
+	assert.NilError(t, err)
+	assert.Check(t, len(creds) == 2)
+	assert.Check(t, creds[0] == "DasUberApiKey")
+	assert.Check(t, creds[1] == "DasUberApiKey2")
+}
+
+func TestGetCredentialsFromClusterFail(t *testing.T) {
+	listSecretsFunc = func(list *v1.SecretList) error {
+		return fmt.Errorf("something terribly wrong happened")
+	}
+	authCred := NewAuthCredential("X-API-KEY", "custom_header", &mockK8sClient)
+	_, err := authCred.GetCredentialsFromCluster(context.TODO(), map[string]string{})
+
+	assert.Error(t, err, "something terribly wrong happened")
 }
