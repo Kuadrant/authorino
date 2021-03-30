bd55be7a97b80e0ac7c3a7880564f09ba8830fdf
Guilherme Cassolato <guicassolato@gmail.com>
2021-03-30 16:38:53 +0200

    [refactor] Improving readability of KubernetesAuth.Call

--- pkg/config/identity/kubernetes_auth.go
+++ pkg/config/identity/kubernetes_auth.go
@@ -70,18 +70,7 @@ func (kubeAuth *KubernetesAuth) Call(authCtx common.AuthContext, ctx context.Con
 		if result, err := kubeAuth.authenticator.TokenReviews().Create(ctx, &tr, metav1.CreateOptions{}); err != nil {
 			return nil, err
 		} else {
-			resultStatus := result.Status
-			if resultStatus.Authenticated {
-				claims := jwt.MapClaims{}
-				// returns the jwt claims (if the token is in fact a jwt); otherwise, returns the "user" in the TokenReview status
-				if token, _, err := new(jwt.Parser).ParseUnverified(reqToken, claims); err != nil { // we don't care about verifying the jwt because it has been authenticated ("reviewed") already
-					return resultStatus.User, nil
-				} else {
-					return token.Claims, nil
-				}
-			} else {
-				return nil, fmt.Errorf("Not authenticated")
-			}
+			return parseTokenReviewResult(result)
 		}
 	}
 }
@@ -91,5 +80,28 @@ func (kubeAuth *KubernetesAuth) audiencesWithDefault(defaultAudience string) []s
 		return kubeAuth.audiences
 	} else {
 		return []string{defaultAudience}
+	}
+}
+
+func parseTokenReviewResult(tokenReview *authv1.TokenReview) (interface{}, error) {
+	tokenReviewStatus := tokenReview.Status
+	if tokenReviewStatus.Authenticated {
+		// returns the jwt claims (if the token is in fact a jwt); otherwise, returns the "user" in the TokenReview status
+		if claims, err := decodeTrustedJWT(tokenReview.Spec.Token); err == nil {
+			return claims, nil
+		} else {
+			return tokenReviewStatus.User, nil
+		}
+	} else {
+		return nil, fmt.Errorf("Not authenticated")
+	}
+}
+
+func decodeTrustedJWT(rawToken string) (jwt.Claims, error) {
+	claims := jwt.MapClaims{}
+	if token, _, err := new(jwt.Parser).ParseUnverified(rawToken, claims); err != nil { // we don't care about verifying the jwt because it has been authenticated ("reviewed") already
+		return nil, err
+	} else {
+		return token.Claims, nil
 	}
 }
