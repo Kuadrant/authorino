1446323b79d36b1c22405792f65c1a957717b4d9
Guilherme Cassolato <guicassolato@gmail.com>
2021-04-14 19:20:17 +0200

    realm info added to the authentication challenge

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "github.com/3scale-labs/authorino/pkg/common"
+import (
+	"fmt"
+
+	"github.com/3scale-labs/authorino/pkg/common"
+)
 
 // APIConfig holds the static configuration to be evaluated in the auth pipeline
 type APIConfig struct {
@@ -12,10 +16,10 @@ type APIConfig struct {
 func (config *APIConfig) GetChallengeHeaders() []map[string]string {
 	challengeHeaders := make([]map[string]string, 0)
 
-	for _, idConfig := range config.IdentityConfigs {
-		idEvaluator := idConfig.(common.IdentityConfigEvaluator)
-		creds := idEvaluator.GetAuthCredentials()
-		challengeHeaders = append(challengeHeaders, map[string]string{"WWW-Authenticate": creds.GetCredentialsKeySelector()})
+	for _, authConfig := range config.IdentityConfigs {
+		idConfig := authConfig.(*IdentityConfig)
+		challenge := fmt.Sprintf("%v realm=\"%v\"", idConfig.GetAuthCredentials().GetCredentialsKeySelector(), idConfig.Name)
+		challengeHeaders = append(challengeHeaders, map[string]string{"WWW-Authenticate": challenge})
 	}
 
 	return challengeHeaders
