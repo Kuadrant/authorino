f49fa06c3e84a8ca554e151d2bd1ad457d516d4b
Guilherme Cassolato <guicassolato@gmail.com>
2023-02-09 17:35:20 +0100

    lint: fix spacing in comments

--- pkg/context/context.go
+++ pkg/context/context.go
@@ -63,13 +63,13 @@ func New(opts ...option) gocontext.Context {
 // CheckContext checks if a go context is still active or done
 // When it's done, returns a generic error
 //
-// func myFunc(ctx context.Context) error {
-//   if err := common.CheckContext(ctx); err != nil {
-//   	 return err
-//   } else {
-//     doSomething()
-//   }
-// }
+//	func myFunc(ctx context.Context) error {
+//	  if err := common.CheckContext(ctx); err != nil {
+//	  	 return err
+//	  } else {
+//	    doSomething()
+//	  }
+//	}
 func CheckContext(ctx gocontext.Context) error {
 	select {
 	case <-ctx.Done():
