ca0696993b5ff7f7a6eff87fabf9a1701349c77e
Guilherme Cassolato <guicassolato@gmail.com>
2023-06-12 10:11:13 +0200

    refactor: detect standard base64 encoding or use raw std algorithm

--- pkg/json/json.go
+++ pkg/json/json.go
@@ -236,7 +236,7 @@ var extractJSONStr = func(json, arg string) string {
 		return "n"
 	}
 
-	return fmt.Sprintf("\"%s\"", parts[pos])
+	return wrap(parts[pos])
 }
 
 var replaceJSONStr = func(json, arg string) string {
@@ -257,7 +257,7 @@ var replaceJSONStr = func(json, arg string) string {
 	})
 
 	str := gjson.Parse(json).String()
-	return fmt.Sprintf("\"%s\"", strings.ReplaceAll(str, old, new))
+	return wrap(strings.ReplaceAll(str, old, new))
 }
 
 var caseJSONStr = func(json, arg string) string {
@@ -276,14 +276,16 @@ var base64JSONStr = func(json, arg string) string {
 	switch arg {
 	case "encode":
 		encoded := base64.StdEncoding.EncodeToString([]byte(str))
-		return fmt.Sprintf("\"%s\"", encoded)
+		return wrap(encoded)
 	case "decode":
-		encoding := base64.StdEncoding
-		if !strings.HasSuffix(str, "=") {
-			encoding = base64.RawStdEncoding
+		if len(str)%4 == 0 {
+			if decoded, err := base64.StdEncoding.DecodeString(str); err == nil {
+				return wrap(escapeQuotes(string(decoded)))
+			}
 		}
-		decoded, _ := encoding.DecodeString(str)
-		return fmt.Sprintf("\"%s\"", strings.ReplaceAll(string(decoded), `"`, `\"`))
+		// try raw standard encoding if unpadded or error
+		decoded, _ := base64.RawStdEncoding.DecodeString(str)
+		return wrap(escapeQuotes(string(decoded)))
 	default:
 		return json
 	}
@@ -298,6 +300,14 @@ var stripJSONstr = func(json, arg string) string {
 	}, json)
 
 	return json
+}
+
+func wrap(s string) string {
+	return fmt.Sprintf("\"%s\"", s)
+}
+
+func escapeQuotes(s string) string {
+	return strings.ReplaceAll(s, `"`, `\"`)
 }
 
 func init() {
