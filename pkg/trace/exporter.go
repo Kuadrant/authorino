1702fdc1fa9f218d36bc5c89a0d9f370d9edf20e
Guilherme Cassolato <guicassolato@gmail.com>
2024-01-19 14:47:04 +0100

    fix: bump OpenTelemetry Schema URL to 1.21.0
    Fixes an error in the initialization of Authorino with tracing services enabled – "cannot merge resource due to conflicting Schema URL"

--- pkg/trace/exporter.go
+++ pkg/trace/exporter.go
@@ -13,7 +13,7 @@ import (
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 )
 
 type Config struct {
