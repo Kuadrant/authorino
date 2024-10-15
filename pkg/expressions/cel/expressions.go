6087b3c490a2f22db155ede186a0e02a6523759b
Alex Snaps <alex@wcgw.dev>
2024-10-15 08:11:46 -0400

    Add `context` to Cel's context
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- pkg/expressions/cel/expressions.go
+++ pkg/expressions/cel/expressions.go
@@ -14,7 +14,8 @@ import (
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
-const RootBinding = "auth"
+const RootAuthBinding = "auth"
+const RootContextBinding = "context"
 
 type Predicate struct {
 	program cel.Program
@@ -46,7 +47,9 @@ func (p *Predicate) Matches(json string) (bool, error) {
 
 func Compile(expression string, predicate bool, opts ...cel.EnvOption) (cel.Program, error) {
 	envOpts := append([]cel.EnvOption{cel.Declarations(
-		decls.NewConst(RootBinding, decls.NewObjectType("google.protobuf.Struct"), nil))}, opts...)
+		decls.NewConst(RootAuthBinding, decls.NewObjectType("google.protobuf.Struct"), nil),
+		decls.NewConst(RootContextBinding, decls.NewObjectType("google.protobuf.Struct"), nil),
+	)}, opts...)
 	env, env_err := cel.NewEnv(envOpts...)
 	if env_err != nil {
 		return nil, env_err
@@ -94,9 +97,11 @@ func AuthJsonToCel(json string) (map[string]interface{}, error) {
 	if err := jsonpb.Unmarshal(strings.NewReader(json), &data); err != nil {
 		return nil, err
 	}
-	value := data.GetFields()["auth"]
+	auth := data.GetFields()["auth"]
+	context := data.GetFields()["context"]
 	input := map[string]interface{}{
-		RootBinding: value,
+		RootAuthBinding:    auth,
+		RootContextBinding: context,
 	}
 	return input, nil
 }
--- pkg/expressions/cel/expressions_test.go
+++ pkg/expressions/cel/expressions_test.go
@@ -13,7 +13,7 @@ func TestPredicate(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	predicate, err := NewPredicate(`{"prop1": "value1", "prop2": auth.identity.username}`)
+	predicate, err := NewPredicate(`context`)
 	assert.ErrorContains(t, err, "wanted bool output type")
 
 	pipelineMock := mock_auth.NewMockAuthPipeline(ctrl)
@@ -30,6 +30,14 @@ func TestPredicate(t *testing.T) {
 	assert.NilError(t, err)
 
 	pipelineMock.EXPECT().GetAuthorizationJSON().Return(`{"auth":{"identity":{"username":"john","evil": false}}}`)
+	response, err = predicate.Matches(pipelineMock.GetAuthorizationJSON())
+	assert.NilError(t, err)
+	assert.Equal(t, response, true)
+
+	predicate, err = NewPredicate(`context.request.http.method == "GET"`)
+	assert.NilError(t, err)
+
+	pipelineMock.EXPECT().GetAuthorizationJSON().Return(`{"context":{"request":{"http": {"method": "GET"}}}}`)
 	response, err = predicate.Matches(pipelineMock.GetAuthorizationJSON())
 	assert.NilError(t, err)
 	assert.Equal(t, response, true)
