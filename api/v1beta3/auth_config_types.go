82e828f5fb669a644073f75094b91d6f7ec36195
Guilherme Cassolato <guicassolato@gmail.com>
2025-05-29 09:53:40 +0200

    working around the most stupid gofmt rule ever
    Extra tab space added to the beginning of the comment to avoid gofmt rewriting '' (two single quotes) as ” (smart fancy quote.)
    
    Ref.: https://github.com/golang/go/issues/54312
    
    Signed-off-by: Guilherme Cassolato <guicassolato@gmail.com>

--- api/v1beta3/auth_config_types.go
+++ api/v1beta3/auth_config_types.go
@@ -358,7 +358,8 @@ type ApiKeyAuthenticationSpec struct {
 }
 
 // Settings to fetch the JSON Web Key Set (JWKS) for the JWT authentication.
-// +kubebuilder:validation:XValidation:rule="!(has(self.jwksUrl) && self.jwksUrl != '' && has(self.issuerUrl) && self.issuerUrl != '')",message="Use one of: jwksUrl, issuerUrl"
+//
+//	+kubebuilder:validation:XValidation:rule="!(has(self.jwksUrl) && self.jwksUrl != '' && has(self.issuerUrl) && self.issuerUrl != '')",message="Use one of: jwksUrl, issuerUrl"
 type JwtAuthenticationSpec struct {
 	// URL of the JSON Web Key Set (JWKS) endpoint.
 	// Use it for non-OpenID Connect (OIDC) JWT authentication, where the JWKS URL is known beforehand.
@@ -545,7 +546,8 @@ type OAuth2ClientAuthentication struct {
 }
 
 // Settings of the OpendID Connect UserInfo linked to an OIDC-enabled JWT authentication config of this same AuthConfig.
-// +kubebuilder:validation:XValidation:rule="!(has(self.identitySource) && self.identitySource != '' && has(self.userInfoUrl) && self.userInfoUrl != '')",message="Use one of: identitySource, userInfoUrl"
+//
+//	+kubebuilder:validation:XValidation:rule="!(has(self.identitySource) && self.identitySource != '' && has(self.userInfoUrl) && self.userInfoUrl != '')",message="Use one of: identitySource, userInfoUrl"
 type UserInfoMetadataSpec struct {
 	// Name of an OIDC JWT authentication rule whose obtained configuration includes an "userinfo_endpoint" claim.
 	// One of: identitySource, userInfoUrl
