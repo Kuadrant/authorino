e9c14d5e683f0c21a6ef4c262d1ca85c98b5024a
averevki <sandyverevkin@gmail.com>
2026-07-02 00:21:21 +0200

    Add OPA Rego v1 evaluator
    Signed-off-by: averevki <sandyverevkin@gmail.com>

--- controllers/auth_config_controller.go
+++ controllers/auth_config_controller.go
@@ -40,6 +40,7 @@ import (
 	"github.com/kuadrant/authorino/pkg/oauth2"
 	"github.com/kuadrant/authorino/pkg/trace"
 	"github.com/kuadrant/authorino/pkg/utils"
+	opaParser "github.com/open-policy-agent/opa/v1/ast"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
 
@@ -547,6 +548,7 @@ func (r *AuthConfigReconciler) translateAuthConfig(ctx context.Context, authConf
 	ctxWithLogger = log.IntoContext(ctx, log.FromContext(ctx).WithName("authorization"))
 
 	authzIndex := 0
+	regoV0DeprecationLogged := false
 	for authzName, authorization := range authConfig.Spec.Authorization {
 		predicates, err := buildPredicates(authConfig, authorization.Conditions, jsonexp.All)
 		if err != nil {
@@ -579,7 +581,13 @@ func (r *AuthConfigReconciler) translateAuthConfig(ctx context.Context, authConf
 		case api.OpaAuthorization:
 			policyName := authConfig.GetNamespace() + "/" + authConfig.GetName() + "/" + authzName
 			opa := authorization.Opa
+			regoVersion := authorization_evaluators.RegoVersionFromString(opa.RegoVersion)
 			secret := &v1.Secret{}
+
+			if regoVersion == opaParser.RegoV0 && !regoV0DeprecationLogged {
+				log.FromContext(ctxWithLogger).Info("AuthConfig contains OPA policies using deprecated Rego v0 syntax. Consider upgrading to v1 syntax as soon as possible, since v0 syntax is deprecated and may be removed in future releases of Authorino")
+				regoV0DeprecationLogged = true
+			}
 
 			var (
 				sharedSecret   string
@@ -608,7 +616,7 @@ func (r *AuthConfigReconciler) translateAuthConfig(ctx context.Context, authConf
 			}
 
 			var err error
-			translatedAuthorization.OPA, err = authorization_evaluators.NewOPAAuthorization(policyName, opa.Rego, externalSource, opa.AllValues, authzIndex, ctxWithLogger)
+			translatedAuthorization.OPA, err = authorization_evaluators.NewOPAAuthorization(policyName, opa.Rego, externalSource, opa.AllValues, regoVersion, authzIndex, ctxWithLogger)
 			if err != nil {
 				return nil, err
 			}
--- pkg/evaluators/deny_all.go
+++ pkg/evaluators/deny_all.go
@@ -5,13 +5,15 @@ import (
 
 	"github.com/kuadrant/authorino/pkg/auth"
 	"github.com/kuadrant/authorino/pkg/evaluators/authorization"
+
+	opaParser "github.com/open-policy-agent/opa/v1/ast"
 )
 
 func NewDenyAllAuthorization(ctx context.Context, name, policyName string) auth.AuthConfigEvaluator {
 	if policyName == "" {
 		policyName = name
 	}
-	opaDenyAll, _ := authorization.NewOPAAuthorization(policyName, "allow = false", nil, false, 0, ctx)
+	opaDenyAll, _ := authorization.NewOPAAuthorization(policyName, "allow := false", nil, false, opaParser.RegoV1, 0, ctx)
 	return &AuthorizationConfig{
 		Name:     name,
 		Priority: 0,
